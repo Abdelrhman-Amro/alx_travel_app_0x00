0a981557687782d8f005efe86a48b0ef9e4cf417
“Abdelrhman-Amro” <abdamro288dev@gmail.com>
2025-02-02 05:08:53 +0200

    checks

--- alx_travel_app/listings/serializers.py
+++ alx_travel_app/listings/serializers.py
@@ -1,1 +1,6 @@
 from rest_framework import serializers
+
+
+class ListingSerializer(serializers.ModelSerializer):
+    host_name = serializers.CharField(source="host.username", read_only=True)
+    average_rating = serializers.SerializerMethodField()
